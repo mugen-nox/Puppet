b00fb1ef32a5feca2c711ae2d57e48f1a4708340
mugen-nox <hn135coder@gmail.com>
2026-08-23 02:06:56 +0500

    added loading screen

--- scenes/loading_screen.gd
+++ scenes/loading_screen.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	$VideoStreamPlayer.finished.connect(_go_to_menu)
 
 func _go_to_menu() -> void:
-	get_tree().change_scene_to_file("res://scenes/ui/u_ibackground.tscn")
+	get_tree().change_scene_to_file("res://scenes/u_ibackground.tscn")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed or event is InputEventMouseButton and event.pressed:
