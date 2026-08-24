3d7a313d997d12d8d73b4f49f7e4ff3b8bef7402
mugen-nox <hn135coder@gmail.com>
2026-08-24 20:29:05 +0500

    asdf

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,15 +1,22 @@
 extends Control
 
 @onready var volume_slider: HSlider = $VolumeSlider
+@onready var ambient_slider: HSlider = $AmbientSlider
+@onready var creak_slider: HSlider = $CreakSlider
+@onready var ambient_preview: AudioStreamPlayer = $AmbientPreview
+@onready var creak_preview: AudioStreamPlayer = $CreakPreview
 
 func _ready() -> void:
-	var current_db := AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
-	volume_slider.value = db_to_linear(current_db)
-	volume_slider.value_changed.connect(_on_volume_changed)
+	_setup_slider(volume_slider, "Master", null)
+	_setup_slider(ambient_slider, "Ambient", ambient_preview)
+	_setup_slider(creak_slider, "Creak", creak_preview)
 
-func _on_volume_changed(value: float) -> void:
-	var bus_idx := AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_volume_db(bus_idx, linear_to_db(value))
+func _setup_slider(slider: HSlider, bus_name: String, preview: AudioStreamPlayer) -> void:
+	var idx := AudioServer.get_bus_index(bus_name)
+	slider.value = db_to_linear(AudioServer.get_bus_volume_db(idx))
+	slider.value_changed.connect(func(v): AudioServer.set_bus_volume_db(idx, linear_to_db(v)))
+	if preview:
+		slider.drag_started.connect(func(): preview.play())
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("uid://cykqn3id7oux7")
